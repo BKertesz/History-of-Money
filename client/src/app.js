10b300a31383430764eb92406e17a5b8d247b194
BKertesz <kertesz.balazs92@gmail.com>
2018-06-01 15:05:41 +0100

    Added item list view and html tags

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {});
+const ItemListView = require('./views/item_list_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const itemListDOM = document.querySelector('#item-list-view');
+    const itemListView = new ItemListView(itemListDOM);
+    itemListView.bindEvents();
+
+});
---
+++ client/src/views/item_list_view.js
@@ -0,0 +1,44 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+class ItemListView{
+
+  constructor(view){
+    this.view = view;
+  }
+
+  bindEvents(){
+    setupDataListener()
+  }
+
+  setupDataListener(){
+    //Listening till the model publishes all data then populate the story
+    PubSub.subscribe('Items:all-data-ready',(evt)=>{this.populate(evt.details)})
+  }
+
+  populate(items){
+    //This function creates the invidual lines that are displayed in the list
+    //We create an unordered list that we append each new row
+    const fullList = document.createElement('ul')
+    items.forEach((item)=>{
+
+      const itemRow = document.createElement('li')
+      itemRow.textContent = item.name;
+
+      itemRow.addEventListener('click',(evt)=>{this.handleClick(evt)});
+
+      fullList.appendChild(itemRow);
+    })
+    //We add the list to the existing html element
+    this.view.appendChild(fullList);
+  }
+
+  handleClick(evt){
+    //This function passes data back to the model, what item's details to display
+    PubSub.publish('ItemListView:item-selected',evt.target.name.value)
+  }
+
+
+
+}
+
+module.exports = ItemListView;
