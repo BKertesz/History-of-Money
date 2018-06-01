c0c5cf1e381da6d9de832f86b7fe96424fa0bfbb
BKertesz <kertesz.balazs92@gmail.com>
2018-06-01 13:49:30 +0100

    Added seeds and item router

---
+++ server/routers/item_router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ObjectID = require('mongodb').ObjectID;
+
+const itemRouter = function(itemsCollection){
+
+
+  return router;
+}
+
+module.exports = itemRouter;
