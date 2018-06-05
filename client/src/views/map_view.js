d2ae9670695194274ec53739eac1f785d871d7d3
Ryan Mackay <ryanmackay@protonmail.com>
2018-06-05 14:46:34 +0100

    map displays with hard coded data

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -1,12 +1,13 @@
 class MapView{
-  constructor(container){
+  constructor(container, coordinates){
     this.container = container;
+    this.coordinates = coordinates;
     this.map = null;
   }
 
 
   initiliaze () {
-    console.log('map init');
+    // console.log('map init');
     const openStreetMapUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const openStreetMapTileLayer = new L.TileLayer(openStreetMapUrl);
 
@@ -15,30 +16,12 @@ class MapView{
       .setView([51.505, -0.09], 13);
   }
 
-
-
-  // initiliaze(){
-  //   var map = L.map(this.container, {
-  //       center: [51.505, -0.09],
-  //       zoom: 13
-  //   });
-  //
-  //   console.log(container);
-  //
-  //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //   }).addTo(map);
-  //
-  //   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //     maxZoom: 18,
-  //     id: 'mapbox.streets',
-  //     accessToken: mapKey
-  //   }).addTo(mymap);
+  addMarker() {
+    L.marker(coordinates).addTo(this.map);
+  }
 
 
 
   }
-// };
 
 module.exports = MapView;
